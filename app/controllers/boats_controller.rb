2608b3a1c4f00d953bb0bb5cf4342055267298ac
muhiedin_itani <muhiedin.itani@gmail.com>
2021-11-24 19:45:04 -0500

    finish create

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -6,7 +6,15 @@ class BoatsController < ApplicationController
     render json: boats.as_json
   end
   
-  
+  def create
+    boat = Boat.new(
+      brand: params[:brand],
+      length: params[:length],
+      price: params[:price]
+    )
+    boat.save
+    render json: boat.as_json
+  end
   
   
   
